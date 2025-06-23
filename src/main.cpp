fa7b8d73a85f5834924471660454e0349a083f00
~Simon Frei <simon.frei@protonmail.com>
2025-06-23 16:29:48 +0200

    Game controller support

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 #include <Arduino.h>
 #include <BROSE9323.h>
+#include <GameControllers.h>
 
-#define BUZZER 12  // buzzer connection port
-#define left 3     // button connection ports
-#define right 4
-#define change 5
-#define down 6
 #define WHITE 1
 #define DEFAULT_INTERVAL 700
 
@@ -13,7 +9,14 @@
 #define HEIGHT 84  // screen height in pixels
 #define PANEL_WIDTH 28
 
+// shared pins between all controllers
+#define LATCH_PIN 0
+#define CLOCK_PIN 1
+// individual data pin for each controller
+#define DATA_PIN_0 2
+
 BROSE9323 display(HEIGHT, WIDTH, PANEL_WIDTH);
+GameControllers controllers;
 
 // matrix for "S" shape
 const char pieces_S_l[2][2][4] = {{{0, 0, 1, 1}, {0, 1, 1, 2}},
@@ -49,18 +52,18 @@ int click[] = {1047};
 int click_duration[] = {100};
 int erase[] = {2093};
 int erase_duration[] = {100};
-word currentType, nextType, rotation;
+word currentType, rotation;
 short pieceX, pieceY;
+short prevPieceX, prevPieceY;  // Track previous piece position
 short piece[2][4];
+short prevPiece[2][4];  // Track previous piece data
 int interval = DEFAULT_INTERVAL, score;
 long timer, delayer;
 boolean grid[WIDTH][HEIGHT / SIZE];  // Dynamic grid size based on display dimensions
 boolean b1, b2, b3;
+boolean gamePaused = false;  // Track pause state
 
 void breakLine(short line) {
-  tone(BUZZER, erase[0], 1000 / erase_duration[0]);
-  delay(100);
-  noTone(BUZZER);
   for (short y = line; y >= 0; y--) {
     for (short x = 0; x < WIDTH; x++) {
       grid[x][y] = grid[x][y - 1];
@@ -156,22 +159,8 @@ void copyPiece(short piece[2][4], short type, short rotation) {
   }
 }
 
-void generate() {
-  currentType = nextType;
-  nextType = random(TYPES);
-  if (currentType != 5)
-    pieceX = random(WIDTH) / SIZE;
-  else
-    pieceX = random(WIDTH - 2) / SIZE;
-
-  pieceY = 0;
-  rotation = 0;
-  copyPiece(piece, currentType, rotation);
-}
-
-void drawPiece(short type, short rotation, short x, short y) {
-  for (short i = 0; i < 4; i++)
-    display.fillRect(MARGIN_LEFT + ((HEIGHT / SIZE - 1 - (y + piece[1][i])) * 2), MARGIN_TOP + ((x + piece[0][i]) * 2), SIZE, SIZE, WHITE);
+long getNextType() {
+  return random(0, TYPES);
 }
 
 short getMaxRotation(short type) {
@@ -183,6 +172,24 @@ short getMaxRotation(short type) {
     return 1;
   else
     return 0;
+}
+
+void generate() {
+  randomSeed(millis());
+  currentType = getNextType();
+  if (currentType != 5)
+    pieceX = random(WIDTH) / SIZE;
+  else
+    pieceX = random(WIDTH - 2) / SIZE;
+
+  pieceY = 0;
+  rotation = random(0, getMaxRotation(currentType) - 1);
+  copyPiece(piece, currentType, rotation);
+}
+
+void drawPiece(short x, short y) {
+  for (short i = 0; i < 4; i++)
+    display.fillRect(MARGIN_LEFT + ((HEIGHT / SIZE - 1 - (y + piece[1][i])) * 2), MARGIN_TOP + ((x + piece[0][i]) * 2), SIZE, SIZE, WHITE);
 }
 
 boolean canRotate(short rotation) {
@@ -201,41 +208,131 @@ short getNumberLength(int n) {
 }
 
 void refresh() {
-  display.fillScreen(0);
-  display.display();
+  // Clear previous piece position using previous piece data
+  for (short i = 0; i < 4; i++) {
+    short x = prevPieceX + prevPiece[0][i];
+    short y = prevPieceY + prevPiece[1][i];
+    // Only clear if this position is not part of the grid
+    if (x >= 0 && x < WIDTH / SIZE && y >= 0 && y < HEIGHT / SIZE && !grid[x][y]) {
+      display.fillRect(MARGIN_LEFT + ((HEIGHT / SIZE - 1 - y) * 2), MARGIN_TOP + (x * 2), SIZE, SIZE, 0);
+    }
+  }
+
+  // Draw grid (only the parts that are set)
   drawGrid();
-  drawPiece(currentType, 0, pieceX, pieceY);
+
+  // Draw current piece (only if not paused)
+  if (!gamePaused) {
+    drawPiece(pieceX, pieceY);
+  }
+
+  // Draw pause indicator if paused
+  if (gamePaused) {
+    // Draw "PAUSE" text or indicator
+    for (int i = 0; i < 5; i++) {
+      display.fillRect(MARGIN_LEFT + (i * 4), MARGIN_TOP + 40, 2, 2, WHITE);
+    }
+  }
+
+  // Update previous position and piece data
+  prevPieceX = pieceX;
+  prevPieceY = pieceY;
+  for (short i = 0; i < 4; i++) {
+    prevPiece[0][i] = piece[0][i];
+    prevPiece[1][i] = piece[1][i];
+  }
+
   display.display();
 }
 
+void clearDisplay() {
+  display.fillScreen(1);
+  display.display();
+  delay(100);
+  display.fillScreen(0);
+  display.display();
+}
+
+void initializePieceData() {
+  prevPieceX = pieceX;
+  prevPieceY = pieceY;
+  // Initialize previous piece data
+  for (short i = 0; i < 4; i++) {
+    prevPiece[0][i] = piece[0][i];
+    prevPiece[1][i] = piece[1][i];
+  }
+}
+
+void startNewGame() {
+  // Generate new piece
+  clearDisplay();
+  generate();
+  initializePieceData();
+  timer = millis();
+}
+
+void resetGame() {
+  // Clear the grid
+  for (short x = 0; x < WIDTH; x++) {
+    for (short y = 0; y < HEIGHT / SIZE; y++) {
+      grid[x][y] = 0;
+    }
+  }
+
+  // Reset game variables
+  score = 0;
+  interval = DEFAULT_INTERVAL;
+  gamePaused = false;
+
+  // Start new game
+  startNewGame();
+}
+
 void setup() {
-  pinMode(left, INPUT_PULLUP);
-  pinMode(right, INPUT_PULLUP);
-  pinMode(change, INPUT_PULLUP);
-  pinMode(down, INPUT_PULLUP);
-  pinMode(BUZZER, OUTPUT);
+  // initialize shared pins
+  controllers.init(LATCH_PIN, CLOCK_PIN);
+  // activate first controller ans set the type to SNES
+  controllers.setController(0, GameControllers::SNES, DATA_PIN_0);
 
   display.begin();
   delay(1000);
 
-  display.fillScreen(1);
-  display.display();
-  display.fillScreen(0);
-  display.display();
-
   randomSeed(analogRead(0));
-  nextType = random(TYPES);
-  generate();
-  timer = millis();
+  startNewGame();
 }
 
 void loop() {
+  controllers.poll();
+
+  // Handle pause mode
+  if (controllers.down(0, GameControllers::START)) {
+    gamePaused = !gamePaused;  // Toggle pause state
+    delay(200);                // Debounce
+  }
+
+  // If paused, only handle SELECT for reset
+  if (gamePaused) {
+    if (controllers.down(0, GameControllers::SELECT)) {
+      resetGame();
+      delay(200);  // Debounce
+    }
+    return;  // Exit early, don't run game logic
+  }
+
   if (millis() - timer > interval) {
     checkLines();
     refresh();
     if (nextCollision()) {
       for (short i = 0; i < 4; i++)
         grid[pieceX + piece[0][i]][pieceY + piece[1][i]] = 1;
+      // Clear the piece trail since it's now part of the grid
+      for (short i = 0; i < 4; i++) {
+        short x = prevPieceX + prevPiece[0][i];
+        short y = prevPieceY + prevPiece[1][i];
+        if (x >= 0 && x < WIDTH / SIZE && y >= 0 && y < HEIGHT / SIZE && !grid[x][y]) {
+          display.fillRect(MARGIN_LEFT + ((HEIGHT / SIZE - 1 - y) * 2), MARGIN_TOP + (x * 2), SIZE, SIZE, 0);
+        }
+      }
       generate();
     } else
       pieceY++;
@@ -243,10 +340,7 @@ void loop() {
   }
 
   // when "left" button is pressed
-  if (!digitalRead(left)) {
-    tone(BUZZER, click[0], 1000 / click_duration[0]);
-    delay(100);
-    noTone(BUZZER);
+  if (controllers.down(0, GameControllers::LEFT)) {
     if (b1) {
       if (!nextHorizontalCollision(piece, -1)) {
         pieceX--;
@@ -259,10 +353,7 @@ void loop() {
   }
 
   // when "right" button is pressed
-  if (!digitalRead(right)) {
-    tone(BUZZER, click[0], 1000 / click_duration[0]);
-    delay(100);
-    noTone(BUZZER);
+  if (controllers.down(0, GameControllers::RIGHT)) {
     if (b2) {
       if (!nextHorizontalCollision(piece, 1)) {
         pieceX++;
@@ -275,17 +366,14 @@ void loop() {
   }
 
   // when "down" button is pressed
-  if (digitalRead(down)) {
+  if (controllers.down(0, GameControllers::DOWN)) {
     interval = 20;
   } else {
     interval = DEFAULT_INTERVAL;
   }
 
   // when "change" button is pressed
-  if (!digitalRead(change)) {
-    tone(BUZZER, click[0], 1000 / click_duration[0]);
-    delay(100);
-    noTone(BUZZER);
+  if (controllers.down(0, GameControllers::A)) {
     if (b3) {
       if (rotation == getMaxRotation(currentType) - 1 && canRotate(0)) {
         rotation = 0;
